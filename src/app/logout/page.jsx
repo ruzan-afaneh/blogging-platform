dfeeb9c82b88f03d75003771cd369c9d8b5c31ea
ruzan-afaneh <ruzanafaneh98@gmail.com>
2024-12-26 23:12:46 +0300

    fix build

--- src/app/logout/page.jsx
+++ src/app/logout/page.jsx
@@ -1,4 +1,4 @@
-import { supabase } from '../utils/supabaseClient';
+import { supabase } from '../../utils/supabaseClient';
 
 export default function LogoutButton() {
   const handleLogout = async () => {
